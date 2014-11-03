41ccbc3d5b7bdfc9afaf0ab7849358e3ca89dcdf
DeepSOIC <vv.titov@gmail.com>
2014-11-03 05:43:41 +0400

    AngleViaPoint: proper partial derivatives

--- src/Mod/Sketcher/App/freegcs/Geo.cpp
+++ src/Mod/Sketcher/App/freegcs/Geo.cpp
@@ -1,4 +1,9 @@
 
+#define DEBUG_DERIVS 0
+#if DEBUG_DERIVS
+#include <cassert>
+#endif
+
 #include "Geo.h"
 namespace GCS{
 
@@ -151,20 +156,65 @@ Vector2D Ellipse::CalculateNormal(Point &p, double* derivparam)
 
     Vector2D f2v ( 2*cv.x - f1v.x, 2*cv.y - f1v.y ); //position of focus2
     if(derivparam){
-        //use numeric derivatives for testing
+
+        Vector2D dc;
+        Vector2D df1;
+        Vector2D dp;
+
+        if (derivparam == center.x) dc.x = 1.0;
+        if (derivparam == center.y) dc.y = 1.0;
+        if (derivparam == focus1X) df1.x = 1.0;
+        if (derivparam == focus1Y) df1.y = 1.0;
+        if (derivparam == p.x) dp.x = 1.0;
+        if (derivparam == p.y) dp.y = 1.0;
+        //todo: exit if all are zero
+
+        Vector2D pf1 = Vector2D(pv.x - f1v.x, pv.y - f1v.y);//same as during error function calculation. I reuse the values during derivative calculation
+        Vector2D pf2 = Vector2D(pv.x - f2v.x, pv.y - f2v.y);
+        Vector2D pf1e = pf1.getNormalized();
+        Vector2D pf2e = pf2.getNormalized();
+
+        Vector2D df2 (2*dc.x - df1.x, 2*dc.y - df1.y );
+
+        Vector2D dpf1 (dp.x - df1.x, dp.y - df1.y);//derivative before normalization
+        Vector2D dpf1e (dpf1.x/pf1.length(), dpf1.y/pf1.length());//first portion of normalization derivative (normalized' = unnormalized'/len + unnormalized*(1/len)')
+        dpf1e.x += -pf1.x/pow(pf1.length(),2)*(dpf1.x*pf1e.x + dpf1.y*pf1e.y);//second part of normalization dreivative
+        dpf1e.y += -pf1.y/pow(pf1.length(),2)*(dpf1.x*pf1e.x + dpf1.y*pf1e.y);
+
+        Vector2D dpf2 (dp.x - df2.x, dp.y - df2.y);//same stuff for pf2
+        Vector2D dpf2e (dpf2.x/pf2.length(), dpf2.y/pf2.length());//first portion of normalization derivative (normalized' = unnormalized'/len + unnormalized*(1/len)')
+        dpf2e.x += -pf2.x/pow(pf2.length(),2)*(dpf2.x*pf2e.x + dpf2.y*pf2e.y);//second part of normalization dreivative
+        dpf2e.y += -pf2.y/pow(pf2.length(),2)*(dpf2.x*pf2e.x + dpf2.y*pf2e.y);
+
+        ret.x = dpf1e.x + dpf2e.x;
+        ret.y = dpf1e.y + dpf2e.y;//DeepSOIC: derivative calculated manually... error-prone =) Tested, fixed, looks good.
+
+//numeric derivatives for testing
+#if 0 //make sure to enable DEBUG_DERIVS when enabling
         double const eps = 0.00001;
         double oldparam = *derivparam;
-        Vector2D v1 = this->CalculateNormal(p);
+        Vector2D v0 = this->CalculateNormal(p);
         *derivparam += eps;
-        Vector2D v2 = this->CalculateNormal(p);
+        Vector2D vr = this->CalculateNormal(p);
+        *derivparam = oldparam - eps;
+        Vector2D vl = this->CalculateNormal(p);
         *derivparam = oldparam;
-        ret.x = (v2.x - v1.x) / eps;
-        ret.y = (v2.y - v1.y) / eps;
+        //If not nasty, real derivative should be between left one and right one
+        Vector2D numretl ((v0.x-vl.x)/eps, (v0.y-vl.y)/eps);
+        Vector2D numretr ((vr.x-v0.x)/eps, (vr.y-v0.y)/eps);
+        assert(ret.x <= std::max(numretl.x,numretr.x) );
+        assert(ret.x >= std::min(numretl.x,numretr.x) );
+        assert(ret.y <= std::max(numretl.y,numretr.y) );
+        assert(ret.y >= std::min(numretl.y,numretr.y) );
+#endif
+
     } else {
-        Vector2D pf1 = Vector2D(pv.x - f1v.x, pv.y - f1v.y).getNormalized();
-        Vector2D pf2 = Vector2D(pv.x - f2v.x, pv.y - f2v.y).getNormalized();
-        ret.x = pf1.x + pf2.x;
-        ret.y = pf1.y + pf2.y;
+        Vector2D pf1 = Vector2D(pv.x - f1v.x, pv.y - f1v.y);
+        Vector2D pf2 = Vector2D(pv.x - f2v.x, pv.y - f2v.y);
+        Vector2D pf1e = pf1.getNormalized();
+        Vector2D pf2e = pf2.getNormalized();
+        ret.x = pf1e.x + pf2e.x;
+        ret.y = pf1e.y + pf2e.y;
     };
 
     return ret;
