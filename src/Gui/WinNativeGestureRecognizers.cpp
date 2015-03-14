ba201d96c89e3cff32038648f9fc5ccc9defee24
DeepSOIC <vv.titov@gmail.com>
2015-03-14 19:06:13 +0300

    Windows pinch gesture translator

---
+++ src/Gui/WinNativeGestureRecognizers.cpp
@@ -0,0 +1,154 @@
+/***************************************************************************
+ *   Copyright (c) Victor Titov (DeepSOIC)                                 *
+ *                                           (vv.titov@gmail.com) 2014     *
+ *                                                                         *
+ *   This file is part of the FreeCAD CAx development system.              *
+ *                                                                         *
+ *   This library is free software; you can redistribute it and/or         *
+ *   modify it under the terms of the GNU Library General Public           *
+ *   License as published by the Free Software Foundation; either          *
+ *   version 2 of the License, or (at your option) any later version.      *
+ *                                                                         *
+ *   This library  is distributed in the hope that it will be useful,      *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU Library General Public License for more details.                  *
+ *                                                                         *
+ *   You should have received a copy of the GNU Library General Public     *
+ *   License along with this library; see the file COPYING.LIB. If not,    *
+ *   write to the Free Software Foundation, Inc., 59 Temple Place,         *
+ *   Suite 330, Boston, MA  02111-1307, USA                                *
+ *                                                                         *
+ ***************************************************************************/
+
+#include "WinNativeGestureRecognizers.h"
+
+#include <qevent.h>
+#include <qgraphicsitem.h>
+#include <qgesture.h>
+
+#include <private/qevent_p.h>
+
+QT_BEGIN_NAMESPACE
+
+#if !defined(QT_NO_NATIVE_GESTURES)
+
+QGesture* WinNativeGestureRecognizerPinch::create(QObject* target)
+{
+  if (!target)
+      return new QPinchGestureN; // a special case
+  if (!target->isWidgetType())
+      return 0;
+  if (qobject_cast<QGraphicsObject *>(target))
+      return 0;
+
+ /* QWidget *q = static_cast<QWidget *>(target);
+  QWidgetPrivate *d = q->d_func();
+  d->nativeGesturePanEnabled = true;
+  d->winSetupGestures();*/ //fails to compile =(, but we can rely on this being done by grabGesture(Pan...
+
+  return new QPinchGestureN;
+}
+
+QGestureRecognizer::Result WinNativeGestureRecognizerPinch::recognize(QGesture *gesture, QObject *watched, QEvent *event)
+{
+    QPinchGestureN* q = static_cast<QPinchGestureN*> (gesture);
+    //QPinchGesturePrivate* d = q->d_func();//this fails to compile =( But we can get away without it.
+
+    QGestureRecognizer::Result result = QGestureRecognizer::Ignore;
+    if (event->type() == QEvent::NativeGesture) {
+        QNativeGestureEvent *ev = static_cast<QNativeGestureEvent*>(event);
+        switch(ev->gestureType) {
+        case QNativeGestureEvent::GestureBegin:
+            break;
+        case QNativeGestureEvent::Zoom:
+            result = QGestureRecognizer::TriggerGesture;
+            event->accept();
+            break;
+        case QNativeGestureEvent::GestureEnd:
+            if (q->state() == Qt::NoGesture)
+                return QGestureRecognizer::Ignore; // some other gesture has ended
+            result = QGestureRecognizer::FinishGesture;
+            break;
+        default:
+            return QGestureRecognizer::Ignore;
+        }
+        if (q->state() == Qt::NoGesture) {
+            //start of a new gesture, prefill stuff
+            //d->isNewSequence = true;
+            q->setTotalChangeFlags(0); q->setChangeFlags(0);
+
+            q->setLastCenterPoint(QPointF());
+            q->setCenterPoint(
+                  QPointF(
+                    qreal(ev->position.x()),
+                    qreal(ev->position.y())
+                  )
+            );
+            q->setStartCenterPoint(q->centerPoint());
+            q->setTotalRotationAngle(0.0); q->setLastRotationAngle(0.0);  q->setRotationAngle(0.0);
+            q->setTotalScaleFactor(1.0); q->setLastScaleFactor(1.0); q->setScaleFactor(1.0);
+            q->lastFingerDistance = ev->argument;
+            q->fingerDistance = ev->argument;
+        } else {//in the middle of gesture
+
+            //store new last values
+            q->setLastCenterPoint(q->centerPoint());
+            q->setLastRotationAngle(q->rotationAngle());
+            q->setLastScaleFactor(q->scaleFactor());
+            q->lastFingerDistance = q->fingerDistance;
+
+            //update the current values
+            q->fingerDistance = ev->argument;
+            if(ev->gestureType == QNativeGestureEvent::GestureEnd)
+              q->fingerDistance = q->lastFingerDistance;//the end-of-gesture event holds no finger separation data, hence we are using the last value.
+            q->setScaleFactor(
+                    (qreal)(q->fingerDistance) / (qreal)(q->lastFingerDistance)
+            );
+            q->setCenterPoint(
+                  QPointF(
+                    qreal(ev->position.x()),
+                    qreal(ev->position.y())
+                  )
+            );
+
+            //compute the changes
+            QPinchGesture::ChangeFlags cf = 0;
+            if ( q->scaleFactor() != 1.0 )
+                cf |= QPinchGesture::ScaleFactorChanged;
+            if (q->lastCenterPoint() != q->centerPoint())
+                cf |= QPinchGesture::CenterPointChanged;
+            q->setChangeFlags(cf);
+
+            //increment totals
+            q->setTotalChangeFlags (q->totalChangeFlags() | q->changeFlags());
+            q->setTotalScaleFactor (q->totalScaleFactor() * q->scaleFactor());
+            //d->totalRotationAngle unsupported by Windows =(
+        }
+    }
+    return result;
+}
+
+
+void WinNativeGestureRecognizerPinch::reset(QGesture* gesture)
+{
+  QGestureRecognizer::reset(gesture);//resets the state of the gesture, which is not write-accessible otherwise
+  QPinchGestureN *q = static_cast<QPinchGestureN*>(gesture);
+  q->lastFingerDistance = 0;
+  q->setTotalChangeFlags(0); q->setChangeFlags(0);
+
+  q->setLastCenterPoint(QPointF());
+  q->setCenterPoint(
+        QPointF(
+          0.0,
+          0.0
+        )
+  );
+  q->setStartCenterPoint(q->centerPoint());
+  q->setTotalRotationAngle(0.0); q->setLastRotationAngle(0.0);  q->setRotationAngle(0.0);
+  q->setTotalScaleFactor(1.0); q->setLastScaleFactor(1.0); q->setScaleFactor(1.0);
+  q->lastFingerDistance = 0;
+  q->fingerDistance = 0;
+}
+
+#endif //!defined(QT_NO_NATIVE_GESTURES)
