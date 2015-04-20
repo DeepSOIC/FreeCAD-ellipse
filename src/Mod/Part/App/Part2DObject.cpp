8826e2314b30c51fc52c8bb046f9c21dd595e149
DeepSOIC <vv.titov@gmail.com>
2015-04-20 04:36:52 +0300

    Progress on mapping modes

--- src/Mod/Part/App/Part2DObject.cpp
+++ src/Mod/Part/App/Part2DObject.cpp
@@ -26,7 +26,9 @@
 # include <TopoDS_Shape.hxx>
 # include <TopoDS_Face.hxx>
 # include <TopoDS_Edge.hxx>
+# include <TopoDS_Vertex.hxx>
 # include <TopoDS.hxx>
+# include <BRep_Tool.hxx>
 # include <gp_Pln.hxx>
 # include <gp_Ax1.hxx>
 # include <gp_Pnt.hxx>
@@ -52,6 +54,7 @@
 
 #include <App/FeaturePythonPyImp.h>
 #include "Part2DObjectPy.h"
+# include <BRepLProp_SLProps.hxx>
 
 using namespace Part;
 
@@ -59,7 +62,15 @@ const int Part2DObject::H_Axis = -1;
 const int Part2DObject::V_Axis = -2;
 const int Part2DObject::N_Axis = -3;
 
-const char* Part2DObject::eMapModeStrings[]= {"Deactivated","To Flat Face","Tangent plane","Normal to path",NULL};
+const char* Part2DObject::eMapModeStrings[]= {
+    "Deactivated",
+    "To Flat Face",
+    "Tangent plane",
+    "Normal to path",
+    "FrenetNB",
+    "FrenetTN",
+    "FrenetTB",
+    "CenterOfCurvature",NULL};
 
 
 PROPERTY_SOURCE(Part::Part2DObject, Part::Feature)
@@ -92,7 +103,7 @@ void Part2DObject::positionBySupport(void)
         return;
 
 
-    eMapMode mmode = eMapMode(this->MapMode.getValue());
+    const eMapMode mmode = eMapMode(this->MapMode.getValue());
 
     if (mmode != mmDeactivated) {
         //common stuff for all map modes
@@ -103,9 +114,10 @@ void Part2DObject::positionBySupport(void)
         if (shape._Shape.IsNull())
             throw Base::Exception("Support shape is empty!");
 
-        TopoDS_Shape sh0; //the first subshape listed in Support.getSubValues
+        TopoDS_Shape sh0; //the first subshape listed in Support.getSubValues. Can be empty, if wasn't specified.
         try {
-            sh0 = shape.getSubShape(sub[0].c_str());
+            if (sub.size() > 0)
+                sh0 = shape.getSubShape(sub[0].c_str());
         }
         catch (Standard_Failure) {
             throw Base::Exception("Face/Edge/Vertex in support shape doesn't exist!");
@@ -114,7 +126,8 @@ void Part2DObject::positionBySupport(void)
         //variables to derive the actual placement.
         //They are to be set, depending on the mode:
          //to the sketch
-        gp_Dir dir;
+        gp_Dir SketchNormal;
+        gp_Vec SketchXAxis; //if left zero, a guess will be made
         gp_Pnt SketchBasePoint; //where to put the origin of the sketch
 
 
@@ -145,7 +158,7 @@ void Part2DObject::positionBySupport(void)
             gp_Ax1 Normal = plane.Axis();
             if (Reverse)
                 Normal.Reverse();
-            dir = Normal.Direction();
+            SketchNormal = Normal.Direction();
 
 
             gp_Pnt ObjOrg(Place.getPosition().x,Place.getPosition().y,Place.getPosition().z);
@@ -156,9 +169,44 @@ void Part2DObject::positionBySupport(void)
 
         } break;
         case mmTangentPlane: {
-            //todo
-        }
-        case mmNormalToPath: {
+            if (sub.size() < 2)
+                throw Base::Exception("Part2DObject::positionBySupport: second subshape is not specified for TangentPlane alignment mode.");
+            TopoDS_Shape sh1;
+            try {
+                sh1 = shape.getSubShape(sub[1].c_str());
+            }
+            catch (Standard_Failure) {
+                throw Base::Exception("Face/Edge/Vertex in support shape doesn't exist!");
+            }
+
+            const TopoDS_Face &face = TopoDS::Face(sh0);
+            if (face.IsNull())
+                throw Base::Exception("Null face in Part2DObject::positionBySupport()!");
+
+            const TopoDS_Vertex &vertex = TopoDS::Vertex(sh1);
+            if (vertex.IsNull())
+                throw Base::Exception("Null vertex in Part2DObject::positionBySupport()!");
+
+            BRepAdaptor_Surface surf (face);
+            Handle (Geom_Surface) hSurf = BRep_Tool::Surface(face);
+            gp_Pnt p = BRep_Tool::Pnt(vertex);
+
+            GeomAPI_ProjectPointOnSurf projector(p, hSurf);
+            double u, v;
+            if (projector.NbPoints()==0)
+                throw Base::Exception("Part2DObject::positionBySupport: projecting point onto surface failed.");
+            projector.Parameters(1, u, v);
+
+            BRepLProp_SLProps prop(surf,u,v,1, Precision::Confusion());
+            SketchNormal = prop.Normal().Reversed();
+            //TODO: reversed surfaces
+            SketchBasePoint = projector.NearestPoint();
+        } break;
+        case mmNormalToPath:
+        case mmFrenetNB:
+        case mmFrenetTN:
+        case mmFrenetTB:
+        case mmNormalToPathRev: {//all alignments to poing on curve
             const TopoDS_Edge &path = TopoDS::Edge(sh0);
             if (path.IsNull())
                 throw Base::Exception("Null path in Part2DObject::positionBySupport()!");
@@ -171,13 +219,75 @@ void Part2DObject::positionBySupport(void)
             gp_Pnt p;  gp_Vec d; //point and derivative
             adapt.D1(u,p,d);
 
-            dir = gp_Dir(d);
+            if (d.Magnitude()<Precision::Confusion())
+                throw Base::Exception("Part2DObject::positionBySupport: path curve derivative is below 1e-7, too low, can't align");
 
-            gp_Pnt ObjOrg(Place.getPosition().x,Place.getPosition().y,Place.getPosition().z);
+            if (mmode == mmNormalToPathRev
+                    || mmode == mmFrenetNB
+                    || mmode == mmFrenetTN
+                    || mmode == mmFrenetTB){
+                gp_Vec dd;//second derivative
+                try{
+                    adapt.D2(u,p,d,dd);
+                } catch (Standard_Failure &e){
+                    //ignore. This is brobably due to insufficient continuity.
+                    dd = gp_Vec(0., 0., 0.);
+                    Base::Console().Warning("Part2DObject::positionBySupport: can't calculate second derivative of curve. OCC error: %s\n", e.GetMessageString());
+                }
 
-            Handle (Geom_Plane) gPlane = new Geom_Plane(p, dir);
-            GeomAPI_ProjectPointOnSurf projector(ObjOrg,gPlane);
-            SketchBasePoint = projector.NearestPoint();
+                gp_Vec T,N,B;//Frenet?Serret axes: tangent, normal, binormal
+                T = d.Normalized();
+                N = dd.Subtracted(T.Multiplied(dd.Dot(T)));//take away the portion of dd that is along tangent
+                if (N.Magnitude() > Precision::SquareConfusion()) {
+                    N.Normalize();
+                    B = T.Crossed(N);
+                } else {
+                    Base::Console().Warning("Part2DObject::positionBySupport: path curve second derivative is below 1e-14, can't align x axis.\n");
+                    N = gp_Vec(0.,0.,0.);
+                    B = gp_Vec(0.,0.,0.);//redundant, just for consistency
+                }
+
+                SketchBasePoint = p; //align sketch origin to the point the curve pierces the sketch. In mmNormalToPathRev, it will be overwritten later
+
+                switch (mmode){
+                case mmFrenetNB:
+                case mmNormalToPathRev:
+                    SketchNormal = T.Reversed();//to avoid sketches upside-down for regular curves like circles
+                    SketchXAxis = N.Reversed();
+                    if (mmode == mmNormalToPathRev) {
+                        //make sketch Y axis of sketch be the axis of osculating circle
+                        if (N.Magnitude() == 0.0)
+                            throw Base::Exception("Part2DObject::positionBySupport: path has infinite radius of curvature at the point. Can't align for revolving.");
+                        double curvature = dd.Dot(N) / pow(d.Magnitude(), 2);
+                        gp_Vec pv (p.XYZ());
+                        pv.Add(N.Multiplied(1/curvature));//shift the point along curvature by radius of curvature
+                        SketchBasePoint = gp_Pnt(pv.XYZ());
+                        //it would have been cool to have the curve attachment point available inside sketch... Leave for future.
+                    }
+                break;
+                case mmFrenetTN:
+                    if (N.Magnitude() == 0.0)
+                        throw Base::Exception("Part2DObject::positionBySupport: Frenet-Serret normal is undefined. Can't align to TN plane.");
+                    SketchNormal = B;
+                    SketchXAxis = T;
+                break;
+                case mmFrenetTB:
+                    if (N.Magnitude() == 0.0)
+                        throw Base::Exception("Part2DObject::positionBySupport: Frenet-Serret normal is undefined. Can't align to TB plane.");
+                    SketchNormal = N.Reversed();//it is more convenient to sketch on something looking it it so it is convex.
+                    SketchXAxis = T;
+                break;
+                default:
+                    assert(0);
+                }
+            } else if (mmode == mmNormalToPath){//mmNormalToPath
+                //align sketch origin to the origin of support
+                SketchNormal = gp_Dir(d.Reversed());//sketch normal looks at user. It is natural to have the curve directed away from user, so reversed.
+                gp_Pnt ObjOrg(Place.getPosition().x,Place.getPosition().y,Place.getPosition().z);
+                Handle (Geom_Plane) gPlane = new Geom_Plane(p, SketchNormal);
+                GeomAPI_ProjectPointOnSurf projector(ObjOrg,gPlane);
+                SketchBasePoint = projector.NearestPoint();
+            }
 
         } break;
         default:
@@ -188,44 +298,48 @@ void Part2DObject::positionBySupport(void)
 
         //----------calculate placement, based on point and vector.
 
-        //find out, to which axis of support Normal is closest to.
-        //The result will be written into pos variable (0..2 = X..Z)
-        Base::Vector3d dX,dY,dZ;//internal axes of support object, as they are in global space
-        Place.getRotation().multVec(Base::Vector3d(1,0,0),dX);
-        Place.getRotation().multVec(Base::Vector3d(0,1,0),dY);
-        Place.getRotation().multVec(Base::Vector3d(0,0,1),dZ);
-        gp_Dir dirX(dX.x, dX.y, dX.z);
-        gp_Dir dirY(dY.x, dY.y, dY.z);
-        gp_Dir dirZ(dZ.x, dZ.y, dZ.z);
-        double cosNX = dir.Dot(dirX);
-        double cosNY = dir.Dot(dirY);
-        double cosNZ = dir.Dot(dirZ);
-        std::vector<double> cosXYZ;
-        cosXYZ.push_back(fabs(cosNX));
-        cosXYZ.push_back(fabs(cosNY));
-        cosXYZ.push_back(fabs(cosNZ));
-
-        int pos = std::max_element(cosXYZ.begin(), cosXYZ.end()) - cosXYZ.begin();
-
         gp_Ax3 SketchPos;
-        // +X/-X
-        if (pos == 0) {
-            if (cosNX > 0)
-                SketchPos = gp_Ax3(SketchBasePoint, dir, dirY);
-            else
-                SketchPos = gp_Ax3(SketchBasePoint, dir, -dirY);
-        }
-        // +Y/-Y
-        else if (pos == 1) {
-            if (cosNY > 0)
-                SketchPos = gp_Ax3(SketchBasePoint, dir, -dirX);
-            else
-                SketchPos = gp_Ax3(SketchBasePoint, dir, dirX);
-        }
-        // +Z/-Z
-        else {
-            SketchPos = gp_Ax3(SketchBasePoint, dir, dirX);
-        }
+        if (SketchXAxis.Magnitude() > Precision::Confusion()) {
+            SketchPos = gp_Ax3(SketchBasePoint, SketchNormal, SketchXAxis);
+        } else {
+            //find out, to which axis of support Normal is closest to.
+            //The result will be written into pos variable (0..2 = X..Z)
+            Base::Vector3d dX,dY,dZ;//internal axes of support object, as they are in global space
+            Place.getRotation().multVec(Base::Vector3d(1,0,0),dX);
+            Place.getRotation().multVec(Base::Vector3d(0,1,0),dY);
+            Place.getRotation().multVec(Base::Vector3d(0,0,1),dZ);
+            gp_Dir dirX(dX.x, dX.y, dX.z);
+            gp_Dir dirY(dY.x, dY.y, dY.z);
+            gp_Dir dirZ(dZ.x, dZ.y, dZ.z);
+            double cosNX = SketchNormal.Dot(dirX);
+            double cosNY = SketchNormal.Dot(dirY);
+            double cosNZ = SketchNormal.Dot(dirZ);
+            std::vector<double> cosXYZ;
+            cosXYZ.push_back(fabs(cosNX));
+            cosXYZ.push_back(fabs(cosNY));
+            cosXYZ.push_back(fabs(cosNZ));
+
+            int pos = std::max_element(cosXYZ.begin(), cosXYZ.end()) - cosXYZ.begin();
+
+            // +X/-X
+            if (pos == 0) {
+                if (cosNX > 0)
+                    SketchPos = gp_Ax3(SketchBasePoint, SketchNormal, dirY);
+                else
+                    SketchPos = gp_Ax3(SketchBasePoint, SketchNormal, -dirY);
+            }
+            // +Y/-Y
+            else if (pos == 1) {
+                if (cosNY > 0)
+                    SketchPos = gp_Ax3(SketchBasePoint, SketchNormal, -dirX);
+                else
+                    SketchPos = gp_Ax3(SketchBasePoint, SketchNormal, dirX);
+            }
+            // +Z/-Z
+            else {
+                SketchPos = gp_Ax3(SketchBasePoint, SketchNormal, dirX);
+            }
+        } // if SketchXAxis.Magnitude() > Precision::Confusion
 
         gp_Trsf Trf;
         Trf.SetTransformation(SketchPos);
