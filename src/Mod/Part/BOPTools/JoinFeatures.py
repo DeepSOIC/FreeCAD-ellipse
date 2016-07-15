dc64459a89170e0f2a4cb45e4772147b22bedb0d
DeepSOIC <vv.titov@gmail.com>
2016-07-15 19:54:42 +0300

    JoinFeatures: fix importability in no-GUI

--- src/Mod/Part/BOPTools/JoinFeatures.py
+++ src/Mod/Part/BOPTools/JoinFeatures.py
@@ -41,14 +41,14 @@ if FreeCAD.GuiUp:
 #http://forum.freecadweb.org/viewtopic.php?f=22&t=11112&start=30#p90239 )
 try:
     _fromUtf8 = QtCore.QString.fromUtf8
-except AttributeError:
+except NameError:
     def _fromUtf8(s):
         return s
 try:
     _encoding = QtGui.QApplication.UnicodeUTF8
     def _translate(context, text, disambig):
         return QtGui.QApplication.translate(context, text, disambig, _encoding)
-except AttributeError:
+except NameError:
     def _translate(context, text, disambig):
         return QtGui.QApplication.translate(context, text, disambig)
 #--------------------------/translation-related code ----------------------------------------
