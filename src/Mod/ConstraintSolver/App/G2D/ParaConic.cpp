4f5b2949c85b91559d1123b9df63912ffcc81236
DeepSOIC <vv.titov@gmail.com>
2020-04-27 00:04:18 +0300

    ConstraintEqualShape: implement methods in geometry classes

--- src/Mod/ConstraintSolver/App/G2D/ParaConic.cpp
+++ src/Mod/ConstraintSolver/App/G2D/ParaConic.cpp
@@ -28,4 +28,27 @@ Position ParaConic::getFocus2(const ValueSet& vals) const
     return 2 * c  + -1 * f1; // = c - (f1-c)
 }
 
+int ParaConic::equalConstraintRank(ParaGeometry& geom2, bool equalTrim) const
+{
+    equalTrim = equalTrim && !this->isFull() && !static_cast<ParaCurve&>(geom2).isFull();
+    return equalTrim ? 4 : 2;
+}
+
+void ParaConic::equalConstraintError(const ValueSet& vals, Base::DualNumber* returnbuf, ParaGeometry& geom2, bool equalTrim) const
+{
+    ParaConic& other = static_cast<ParaConic&>(geom2);
+    equalTrim = equalTrim && !this->isFull() && other.isFull();
+
+    DualNumber scaling = 1/this->getRMaj(vals);
+
+    int i = 0;
+    returnbuf[i++] = (getRMaj(vals) - other.getRMaj(vals)) * scaling;
+    returnbuf[i++] = (getRMin(vals) - other.getRMin(vals)) * scaling;
+    if (equalTrim) {
+        returnbuf[i++] = vals[this->u0] - vals[other.u0];
+        returnbuf[i++] = vals[this->u1] - vals[other.u1];
+    }
+
+}
+
 
