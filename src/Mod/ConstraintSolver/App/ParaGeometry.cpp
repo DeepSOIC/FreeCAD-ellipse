be77c5f52ffffdfba0eedcddd72d258ff9cac0f2
DeepSOIC <vv.titov@gmail.com>
2020-04-26 04:19:45 +0300

    implement ConstraintEqualShape (interface only)

--- src/Mod/ConstraintSolver/App/ParaGeometry.cpp
+++ src/Mod/ConstraintSolver/App/ParaGeometry.cpp
@@ -6,3 +6,13 @@
 using namespace FCS;
 
 TYPESYSTEM_SOURCE_ABSTRACT(FCS::ParaGeometry, FCS::ParaObject);
+
+int ParaGeometry::equalConstraintRank(ParaGeometry&, bool) const
+{
+    return 0;
+}
+
+void ParaGeometry::equalConstraintError(const ValueSet&, Base::DualNumber*, ParaGeometry&, bool) const
+{
+
+}
